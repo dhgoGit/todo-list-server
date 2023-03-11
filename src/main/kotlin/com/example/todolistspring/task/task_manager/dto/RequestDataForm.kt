6a444eb1bd3b657465fc163cf676ce51c9b149c5
dhgo <castar2001kr@naver.com>
2023-03-12 01:54:17 +0900

    [add] 인터페이스 구상중

--- src/main/kotlin/com/example/todolistspring/task/task_manager/dto/RequestDataForm.kt
+++ src/main/kotlin/com/example/todolistspring/task/task_manager/dto/RequestDataForm.kt
@@ -2,28 +2,29 @@ package com.example.todolistspring.task.task_manager.dto
 
 import com.example.todolistspring.task.task_manager.Task
 import com.example.todolistspring.task.task_manager.enums.TaskResolveOption
+import com.example.todolistspring.task.task_manager.enums.TaskType
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class RequestDataForm(
     @JsonProperty("task_id")
-    val taskId:Int? = null,
+    val taskId: Int? = null,
     @JsonProperty("task")
-    val task:Task? = null,
-
-
+    val task: Task? = null,
+    @JsonProperty("task_type")
+    val taskType: TaskType,
     @JsonProperty("resolve_option")
-    val resolveOption:TaskResolveOption? = null,
-){
-    companion object{
-        fun requireId(requestDataForm: RequestDataForm) = run{
+    val resolveOption: TaskResolveOption? = null,
+) {
+    companion object {
+        fun requireId(requestDataForm: RequestDataForm) = run {
             requestDataForm.taskId!!
         }
 
-        fun requireTask(requestDataForm: RequestDataForm) = run{
+        fun requireTask(requestDataForm: RequestDataForm) = run {
             requestDataForm.task!!
         }
 
-        fun requireResolveOption(requestDataForm: RequestDataForm) = run{
+        fun requireResolveOption(requestDataForm: RequestDataForm) = run {
             requestDataForm.resolveOption!!
         }
     }
